27d7229331732ac9a085152da5dc64a6310b1f30
Jukka Jylänki <jujjyl@gmail.com>
2022-11-23 21:37:24 +0200

    Update simple2.c

--- tests/simple2.c
+++ tests/simple2.c
@@ -15,10 +15,18 @@ int main (void)
   printf("GC heap free before collect: %zu\n", heapBefore);
   GC_gcollect();
   size_t heapAfter = GC_get_free_bytes();
-  printf("GC heap free after  collect: %zu (should be same as before)\n", heapAfter);
+  printf("GC heap free after 1st collect: %zu (should be same as before)\n", heapAfter);
   printf("ptr: %p\n", ptr); // Print the pointer value so -O3 does not optimize it away altogether.
 
-  assert(heapBefore == heapAfter);
+  assert(heapAfter == heapBefore);
+
+  ptr = 0; // let the ptr be reclaimed
+
+  GC_gcollect();
+  heapAfter = GC_get_free_bytes();
+  printf("GC heap free after 2nd collect: %zu (should be more than before)\n", heapAfter);
+
+  assert(heapAfter > heapBefore);
 
   return 0;
 }
